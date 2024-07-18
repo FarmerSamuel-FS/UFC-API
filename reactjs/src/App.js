7b3e94b8ce25539c51324fdfe0af08514b020653
FarmerSamuel-FS <sfarmer1@student.fullsail.edu>
2024-07-17 21:15:10 -0400

    React App git fix

--- reactjs/src/App.js
+++ reactjs/src/App.js
@@ -1,11 +1,10 @@
+import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Fighter from "./pages/Fighter";
 import Dashboard from "./pages/Dashboard";
 
 function App() {
-<<<<<<< HEAD:ufc-react/src/App.js
-=======
   const [fighters, setFighters] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
@@ -15,39 +14,42 @@ function App() {
       ? `http://localhost:8000/api/v1/`
       : process.env.REACT_APP_BASE_URL;
 
-  let ignore = false;
   useEffect(() => {
-    if (!ignore) {
-      getFighters();
-    }
+    let ignore = false;
+
+    const getFighters = async () => {
+      setLoading(true);
+      try {
+        const response = await fetch(`${API_BASE}/fighters`);
+        if (!ignore) {
+          if (!response.ok) {
+            throw new Error("Failed to fetch fighters");
+          }
+          const data = await response.json();
+          setFighters(data);
+        }
+      } catch (error) {
+        setError(error.message || "Unexpected Error");
+      } finally {
+        if (!ignore) {
+          setLoading(false);
+        }
+      }
+    };
+
+    getFighters();
+
     return () => {
       ignore = true;
     };
-  }, []);
+  }, [API_BASE]);
 
-  const getFighters = async () => {
-    setLoading(true);
-    try {
-      await fetch(`${API_BASE}/fighters`)
-        .then((res) => res.json())
-        .then((data) => {
-          console.log({ data });
-          setFighters(data);
-        });
-    } catch (error) {
-      setError(error.message || "Unexpected Error");
-    } finally {
-      setLoading(false);
-    }
-  };
-
->>>>>>> main:reactjs/src/App.js
   return (
     <Router>
       <Routes>
-        <Route path="/" exact element={<Home />} />
-        <Route path="/fighters/:id" exact element={<Fighter />} />
-        <Route path="/dashboard" exact element={<Dashboard />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/fighters/:id" element={<Fighter />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </Router>
   );
